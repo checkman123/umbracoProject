b2426398972bd8e0e5bc4af2705ecf824b8a0753
Nez <s.sirijirakarn@wsu.edu>
2023-02-07 13:36:20 -0800

    add BlockListView to get live preview in umbraco. Cool stuff

---
+++ Composing/StartupComposer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using umbracoProject.NotificationsHandlers;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Notifications;
+
+namespace umbracoProject.Composing
+{
+    internal class StartupComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder
+                .AddNotificationHandler<ServerVariablesParsingNotification,
+                    ServerVariablesParsingNotificationHandler>();
+        }
+    }
+}
